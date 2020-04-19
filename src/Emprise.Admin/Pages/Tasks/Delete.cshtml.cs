c0b922b3575ba16a7378b57a8ecda538c3765c81
alt <8720826@qq.com>
2020-04-19 22:48:45 +0800

    任务删除功能

---
+++ src/Emprise.Admin/Pages/Tasks/Delete.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Emprise.Admin.Data;
+using Emprise.Domain.Tasks.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Emprise.Admin.Pages.Tasks
+{
+    public class DeleteModel : PageModel
+    {
+        protected readonly EmpriseDbContext _db;
+
+        public DeleteModel(EmpriseDbContext db)
+        {
+            _db = db;
+        }
+
+        public TaskEntity Task { get; set; }
+
+        public string SueccessMessage { get; set; }
+        public string ErrorMessage { get; set; }
+
+        [BindProperty]
+        public string UrlReferer { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync(int id = 0)
+        {
+            UrlReferer = Request.Headers["Referer"].ToString();
+            if (string.IsNullOrEmpty(UrlReferer))
+            {
+                UrlReferer = Url.Page("/Script/Index");
+            }
+
+            if (id > 0)
+            {
+                Task = _db.Tasks.Find(id);
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("/Script/Index");
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id = 0)
+        {
+            SueccessMessage = "";
+            ErrorMessage = "";
+            if (!ModelState.IsValid)
+            {
+                ErrorMessage = ModelState.Where(e => e.Value.Errors.Count > 0).Select(e => e.Value.Errors.First().ErrorMessage).First();
+                return Page();
+            }
+            var task = _db.Tasks.Find(id);
+            _db.Tasks.Remove(task);
+            await _db.SaveChangesAsync();
+
+            SueccessMessage = $"删除成功！";
+
+            //return RedirectToPage("Index");
+
+            return Redirect(UrlReferer);
+        }
+    }
+}
