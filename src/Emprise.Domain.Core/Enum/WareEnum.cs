cba78cc729b52eba7658c82cabc30adfc486fde5
alt <8720826@qq.com>
2020-04-28 23:21:15 +0800

    物品表设计

---
+++ src/Emprise.Domain.Core/Enum/WareEnum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emprise.Domain.Core.Enum
+{
+    public enum WareCategoryEnum
+    {
+        丹药 = 101,//增加体力内力生命
+        武器 = 102,//装备后增加攻击、防御
+        服装 = 103,
+        //暗器 = 4,//用来减少对方生命值
+        宝物 = 105,//从商城购买，特殊用途
+        材料 = 106,//挖矿、伐木、采药等获得
+        //宝石 = 7,//由原材料合成，可以增加武器属性
+        宝箱 = 108, //宝箱内由若干其他物品
+        秘籍 = 109, //武功秘籍
+
+        碎片 = 110,
+
+
+        //工具 = 111,//用来挖矿、伐木、采药等
+        //毒药=112
+    }
+
+
+    public enum WareTypeEnum
+    {
+        刀 = 102001,
+        剑 = 102002,
+        枪 = 102003,
+
+
+
+        衣服 = 103001,
+        帽 = 103002,
+        鞋 = 103003,
+
+
+        秘籍残卷 =110001,
+
+    }
+
+    public enum WareStatusEnum
+    {
+        装备 = 1,
+        卸下 = 0,
+        寄售 = 2
+    }
+}
--- src/Emprise.Domain/Ware/Entity/WareEntity.cs
+++ src/Emprise.Domain/Ware/Entity/WareEntity.cs
@@ -1,4 +1,5 @@
 ﻿using Emprise.Domain.Core.Entity;
+using Emprise.Domain.Core.Enum;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -18,9 +19,37 @@ namespace Emprise.Domain.Ware.Entity
         public string Name { set; get; }
 
         /// <summary>
+        /// 类型，一级分类
+        /// </summary>
+
+        public WareCategoryEnum Category { set; get; }
+
+        /// <summary>
+        /// 类型，二级分类
+        /// </summary>
+
+        public WareTypeEnum Type { set; get; }
+
+
+        /// <summary>
+        /// 效果，使用后产生，使用后物品会被消耗
+        /// </summary>
+        public string Effect { set; get; }
+
+
+
+        /// <summary>
         /// 描述
         /// </summary>
         [StringLength(500)]
         public string Description { set; get; }
+
+
+        /// <summary>
+        /// 图片
+        /// </summary>
+        [StringLength(500)]
+        public string Img { set; get; }
+
     }
 }
